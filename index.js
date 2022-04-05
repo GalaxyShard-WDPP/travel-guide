f00b4b53c6bfe8a5e23530f5524a8cce208fa56b
GalaxyShard-WDPP <99682519+GalaxyShard-WDPP@users.noreply.github.com>
2022-04-05 10:36:30 -0400

    _

--- index.js
+++ index.js
@@ -2,15 +2,13 @@
 
 // Key is from MobileCSP
 var key = "8bb5e8bedfe6fe3f1a44e0a2c04b6540";
-    var text = document.getElementById("weather-info");
+var text = document.getElementById("temperature");
 
 fetch(
-    'https://api.openweathermap.org/data/2.5/weather?lat=20.7503&lon=-156.5003&appid='+key
+    'https://api.openweathermap.org/data/2.5/weather?lat=20.7503&lon=-156.5003&units=metric&appid='+key
     ).then(function(resp){return resp.json();}
 ).then(function(data){
-    // Kelvin to Celcius
-	var temp = Math.round(parseFloat(data.main.temp)-273.15);
-    text.innerHTML = "It is currently "+temp+"°C in Hawaii";
+    text.innerHTML = Math.round(data.main.temp);
 }).catch(function(){
-    
+    console.log("Failed to load weather data");
 });
---
+++ nav.js
@@ -0,0 +1,6 @@
+
+var navItems = [
+    {name:"Tips",link:"tips.html"},
+    {name:"Hawaii",link:"index.html"},
+    {name:"Hotels",link:"hotels.html"}
+];
