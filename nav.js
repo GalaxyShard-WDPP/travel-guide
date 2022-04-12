98983ebfc04d0aecfbdc7cf5e638ae647263d286
GalaxyShard <swift.galaxyshard@gmail.com>
2022-04-12 17:47:14 -0400

    _

--- nav.js
+++ nav.js
@@ -1,8 +1,3 @@
-
-// <!-- <a href="tips.html">Tips</a> -->
-// <!-- <a href="index.html" class="title cursive">Hawaii</a> -->
-// <!-- <a href="hotels.html">Hotels</a> -->
-
 var nav = document.getElementById("nav");
 var links = [
     {name:"Tips", link:"tips.html"},
@@ -10,10 +5,8 @@ var links = [
     {name:"Hotels", link:"hotels.html"}
 ];
 var extra = [
-    {name:"See the official site!", link:"https://www.gohawaii.com/"},
     {name:"Food", link:"food.html"},
-    {name:"Activities", link:"activies.html"}
-
+    {name:"Places", link:"places.html"}
 ];
 var flag = document.createElement("img");
 flag.src = "https://upload.wikimedia.org/wikipedia/commons/e/ef/Flag_of_Hawaii.svg";
@@ -30,13 +23,10 @@ for (let i = 0; i < extra.length; ++i)
     let link = document.createElement("a");
     link.innerHTML = extra[i].name;
     link.href = extra[i].link;
-    link.className = extra[i].class;
     dropContent.appendChild(link);
-    dropContent.appendChild(document.createElement("hr"));
+    if (i != extra.length-1)
+        dropContent.appendChild(document.createElement("hr"));
 }
-// dropContent.innerHTML = "<a href=\"https://www.gohawaii.com/\">See the official site!</a>";
-// var ;
-// dropContent.appendChild();
 drop.appendChild(dropContent);
 nav.appendChild(drop);
 
