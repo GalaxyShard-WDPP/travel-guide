606874b1059a025acf78b971a7236277ce85d1b4
GalaxyShard-WDPP <99682519+GalaxyShard-WDPP@users.noreply.github.com>
2022-04-12 14:58:41 -0400

    _

--- nav.js
+++ nav.js
@@ -9,6 +9,36 @@ var links = [
     {name:"Hawaii", link:"index.html", class:"title cursive"},
     {name:"Hotels", link:"hotels.html"}
 ];
+var extra = [
+    {name:"See the official site!", link:"https://www.gohawaii.com/"},
+    {name:"Food", link:"food.html"},
+    {name:"Activities", link:"activies.html"}
+
+];
+var flag = document.createElement("img");
+flag.src = "https://upload.wikimedia.org/wikipedia/commons/e/ef/Flag_of_Hawaii.svg";
+flag.id = "flag";
+nav.appendChild(flag);
+
+var drop = document.createElement("div");
+drop.id = "nav-dropdown";
+drop.innerHTML = "More";
+var dropContent = document.createElement("div");
+
+for (let i = 0; i < extra.length; ++i)
+{
+    let link = document.createElement("a");
+    link.innerHTML = extra[i].name;
+    link.href = extra[i].link;
+    link.className = extra[i].class;
+    dropContent.appendChild(link);
+    dropContent.appendChild(document.createElement("hr"));
+}
+// dropContent.innerHTML = "<a href=\"https://www.gohawaii.com/\">See the official site!</a>";
+// var ;
+// dropContent.appendChild();
+drop.appendChild(dropContent);
+nav.appendChild(drop);
 
 for (let i = 0; i < links.length; ++i)
 {
@@ -18,9 +48,3 @@ for (let i = 0; i < links.length; ++i)
     link.className = links[i].class;
     nav.appendChild(link);
 }
-
-// nav.innerHTML =
-//     "<a href=\"tips.html\">Tips</a>" +
-//     "<a href=\"index.html\" class=\"title cursive\">Hawaii</a>" +
-//     "<a href=\"hotels.html\">Hotels</a>";
-
